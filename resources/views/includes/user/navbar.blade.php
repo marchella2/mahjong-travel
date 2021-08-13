ea8837926d550ec490bf13e9a732e569b59c87f3
Marchella Putri Sannie <marchella.putris2@gmail.com>
2021-08-13 21:32:56 +0700

    CRUD Paket Travel

--- resources/views/includes/user/navbar.blade.php
+++ resources/views/includes/user/navbar.blade.php
@@ -35,19 +35,40 @@
                     <a href="#" class="nav-link">Testimonial</a>
                 </li>
             </ul>
-            <!-- Mobile Button -->
-            <form action="#" class="form-inline d-sm-block d-md-none">
-                <button class="btn btn-login my-2 my-sm-0 px-4">
-                    Masuk
-                </button>
-            </form>
+           
+            @guest
+                <!-- Mobile Button -->
+                <form action="#" class="form-inline d-sm-block d-md-none">
+                    <button class="btn btn-login my-2 my-sm-0 px-4" type="button" onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                        Masuk
+                    </button>
+                </form>
 
-            <!-- Desktop Button -->
-            <form action="#" class="form-inline my-2 my-lg-0 d-none d-md-block">
-                <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-                    Masuk
-                </button>
-            </form>
+                <!-- Desktop Button -->
+                <form action="#" class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button" onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                        Masuk
+                    </button>
+                </form>
+            @endguest
+
+            @auth
+                <!-- Mobile Button -->
+                <form action="{{ url('logout') }}" class="form-inline d-sm-block d-md-none" method="POST">
+                    @csrf
+                    <button class="btn btn-login my-2 my-sm-0 px-4" type="submit">
+                        Keluar
+                    </button>
+                </form>
+
+                <!-- Desktop Button -->
+                <form action="{{ url('logout') }}" method="POST" class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    @csrf
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+                        Keluar
+                    </button>
+                </form>
+            @endauth
         </div>
     </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,16 @@
 */
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/detail', 'DetailController@index')->name('detail');
 Route::get('/checkout', 'CheckoutController@index')->name('checkout');
 Route::get('/checkout/success', 'CheckoutController@success')->name('checkout-success');
 
-Route::prefix('admin')->namespace('Admin')->group(function() {
+Route::prefix('admin')->namespace('Admin')->middleware(['auth', 'admin'])->group(function() {
     Route::get('/', 'DashboardController@index')->name('dashboard');
+
+    // Travel Package
+    Route::resource('travel-package', 'TravelPackageController');
 });
